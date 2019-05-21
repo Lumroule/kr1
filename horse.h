76c525082218b3a19b32d11c06e4d81fd2d586ba
Lumroule <43366140+Lumroule@users.noreply.github.com>
2019-05-21 08:34:03 +0300

    Create horse.h

---
+++ horse.h
@@ -0,0 +1,31 @@
+horse.h
+
+#pragma once
+
+#include <string>
+#include <fstream>
+#include "function.h"
+
+class horse {
+public:
+	horse();
+	horse(const horse &hrs);
+
+	horse& operator=(const horse &hrs);
+	
+	const std::string& getBreed() const;
+	const std::string& getName() const;
+	size_t getYear() const;
+	size_t getNumWins() const;
+
+	void setNumWins(size_t numWins);
+
+	friend std::ifstream& operator>>(std::ifstream &ifs, horse &hrs);
+	friend bool operator==(const horse &hrs1, const horse &hrs2);
+
+private:
+	std::string breed;
+	std::string name;
+	size_t year;
+	size_t numWins;
+};
